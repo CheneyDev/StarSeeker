4c9842bdd35271be6eea0feef9c43d7b6d080974
CheneyDev <cheneydev@gmail.com>
2025-01-05 22:02:14 +0700

    refactor(search_box): simplify search box structure and remove leading visual elements
    - Remove leading visual elements from the search box for a cleaner design
    - Update CSS to eliminate unnecessary styles related to leading visuals
    - Streamline the input wrap structure for improved accessibility and usability

--- components/search_box.js
+++ components/search_box.js
@@ -26,12 +26,7 @@ class SearchBox {
             <label class="sr-only FormControl-label" for="star-search">
               Search stars
             </label>    
-            <div class="FormControl-input-wrap FormControl-input-wrap--leadingVisual">
-              <span class="FormControl-input-leadingVisualWrap">
-                <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-search FormControl-input-leadingVisual">
-                  <path d="M10.68 11.74a6 6 0 0 1-7.922-8.982 6 6 0 0 1 8.982 7.922l3.04 3.04a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215ZM11.5 7a4.499 4.499 0 1 0-8.997 0A4.499 4.499 0 0 0 11.5 7Z"></path>
-                </svg>
-              </span>
+            <div class="FormControl-input-wrap">
               <input 
                 id="star-search"
                 type="search" 
